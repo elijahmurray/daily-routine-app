24ee685bc4e6af7f8a059c0a9a03e5f05827e19d
Elijah Murray <elijahmurray@gmail.com>
2017-03-22 23:29:08 -0400

    show checkbox inline

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -133,4 +133,18 @@ SimpleForm.setup do |config|
     file: :vertical_file_input,
     boolean: :vertical_boolean,
   }
+  config.wrappers :float_left_checkbox, tag: 'div', class: 'form-inline float-left', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :label, class: 'sr-only'
+
+    b.use :input, class: 'form-control'
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+  end
+
 end
