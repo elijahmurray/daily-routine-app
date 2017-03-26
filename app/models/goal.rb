5fe2113eb12a2283733e4b93c6b117c61a7318bc
Elijah Murray <elijahmurray@gmail.com>
2017-03-26 16:46:54 -0400

    setup picklist for time to complete

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,13 @@
 class Goal < ActiveRecord::Base
+  DURATIONS_IN_MONTHS = [1,3,6,12,24,36,60,120]
+
+  def  self.durations
+    DURATIONS_IN_MONTHS.map do |d| 
+      if d < 12
+        d.to_s + " Months"
+      else
+        (d/12).to_s + " Years"
+      end
+    end
+  end
 end
